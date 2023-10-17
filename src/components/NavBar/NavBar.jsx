9904cc8208c874503ca677de740a7ef7a607b229
YASH <yash.1138@zenmonk.tech>
2023-10-17 12:54:56 +0530

    Done with CSS STYLES

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
-import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import useStyles from './styles';
@@ -25,36 +37,27 @@ const NavBar = () => {
               <Menu />
             </IconButton>
           )}
-          <IconButton
-            color="inherit"
-            sx={{ ml: 1 }}
-            onClick={() => {}}
-          >
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
             {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
           {!isMobile && 'Search...'}
           <div>
-           {!isAuthenticated ? 
-           (
-                <Button
-                 color="inherit"
-                 onClick={() => {}} 
-                >
-                 Login &nbsp; <AccountCircle />
-                </Button>
-            )
-            :
-            (
-              //  <Button
-              //   color= "inherit"
-              //   component={Link}
-              //   to={'/profile/:id'}
-              //   >
-              //   Profile
-              //  </Button> 
-            )
-            }
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button color="inherit" component={Link} to="/profile/:id" className={classes.linkButton} onClick={() => {}}>
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
           </div>
+          {isMobile && 'Search...'}
         </Toolbar>
       </AppBar>
     </>
